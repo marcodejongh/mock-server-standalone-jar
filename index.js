f7ab2ead06a5c7b4d39887a969d3a93b0602d753
Marco De Jongh <mdejongh@atlassian.com>
2016-07-28 16:25:56 +0200

    OKAY now it WORKS 🎉

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var json = require('./package.json');
 var path = require('path');
 
 module.exports = {
-  path: path.resolve(__dirname, 'jar/mockserver-netty-3.10.4.jar'),
+  path: path.resolve(__dirname, 'jar/mockserver-netty-3.10.4-jar-with-dependencies.jar'),
   version: '3.10.4'
   //TODO: Get these versions in sync again
-  // path: path.resolve(__dirname, 'jar/mockserver-netty-' + json.version + '.jar'),
+  // path: path.resolve(__dirname, `mockserver-netty-${json.version}-jar-with-dependencies.jar`),
   // version: json.version
 };
