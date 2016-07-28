12262883ebb6cd71f12ac71efbca2860d23abb22
Marco De Jongh <mdejongh@atlassian.com>
2016-07-28 15:54:23 +0200

    Versions no longer in sync

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var json = require('./package.json');
 var path = require('path');
 
 module.exports = {
-  path: path.resolve(__dirname, 'jar/mockserver-netty-' + json.version + '.jar'),
-  version: json.version
+  path: path.resolve(__dirname, 'jar/mockserver-netty-3.10.4.jar'),
+  version: '3.10.4'
+  //TODO: Get these versions in sync again
+  // path: path.resolve(__dirname, 'jar/mockserver-netty-' + json.version + '.jar'),
+  // version: json.version
 };
